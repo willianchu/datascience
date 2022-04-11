d5795eb9ed1f9f3a4ea7a01cfe7d0c8f00ac6648
willianchu <willianchu@hotmail.com>
2022-04-11 12:09:18 -0700

    data flights

---
+++ cheapest_flight.py
@@ -0,0 +1,31 @@
+data = {
+"van_tor_price" :  250,
+"van_ott_price" :  280,
+"van_mon_price" :  240,
+"van_edm_price" :  150,
+"van_cal_price" :  180,
+
+"van_tor_travel_time" :  3.5,
+"van_ott_travel_time" :  4,
+"van_mon_travel_time" :  4,
+"van_edm_travel_time" :  1.5,
+"van_cal_travel_time" :  1,
+
+"ott_ber_price" :  1350,
+"mon_lon_price" :  1300,
+"edm_lon_price" :  1290,
+"cal_lon_price" :  1400,
+"tor_mun_price" :  990,
+
+"ott_layover" :  3.5,
+"mon_layover" :  2,
+"edm_layover" :  5,
+"cal_layover" :  2.5,
+"tor_layover" :  1.5,
+
+"ott_ber_travel_time" :  9,
+"mon_lon_travel_time" :  8,
+"edm_lon_travel_time" :  10,
+"cal_lon_travel_time" :  10,
+"tor_mun_travel_time" :  9.5
+}
