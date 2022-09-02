a94f658f5f37c44d5b9789598427698f2fab7601
willianchu <willianchu@hotmail.com>
2022-09-02 14:19:54 -0700

    adapted answers

---
+++ WeatherObservationStation9.sql
@@ -0,0 +1,16 @@
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+CREATE TABLE STATION (
+  ID number,
+  CITY varchar2(21),
+  STATE varchar2,
+  LAT_N number,
+  LONG_W number
+)
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY, 1) NOT IN ( 'A','E','I','O', 'U');
