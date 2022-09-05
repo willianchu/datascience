934a50370488374ae062888a77d8579b2274c088
willianchu <willianchu@hotmail.com>
2022-09-04 17:29:55 -0700

    agregation

---
+++ TheSumFunction.sql
@@ -0,0 +1,14 @@
+-- Query the total population of all cities in CITY where District is California.
+
+-- Input Format
+
+-- The CITY table is described as follows:
+
+CREATE TABLE CITY (
+  ID  NUMBER,
+  NAME  VARCHAR2(17),
+  COUNTRYCODE VARCHAR2(3),
+  DISTRICT  VARCHAR2(20),
+  POPULATION NUMBER,
+)
+
