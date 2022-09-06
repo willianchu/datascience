518fd541b8f9ed8e889a59fe0908b7d2b8c29bab
willianchu <willianchu@hotmail.com>
2022-09-06 07:45:56 -0700

    avg function

---
+++ Averages.sql
@@ -0,0 +1,16 @@
+-- Query the average population of all cities in CITY where District is California.
+
+-- Input Format
+
+-- The CITY table is described as follows:
+
+CREATE TABLE CITY (
+  ID  NUMBER,
+  NAME  VARCHAR2(17),
+  COUNTRYCODE VARCHAR2(3),
+  DISTRICT  VARCHAR2(20),
+  POPULATION NUMBER,
+)
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE LOWER(district) = 'california';
