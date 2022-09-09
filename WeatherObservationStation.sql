d9ca06032ca7c107eac0c6b00afa934924bd6823
willianchu <willianchu@hotmail.com>
2022-09-09 09:33:59 -0700

    manhatan distance

--- WeatherObservationStation.sql
+++ WeatherObservationStation.sql
@@ -1,8 +1,4 @@
--- <!-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
 
--- Input Format
-
--- The STATION table is described as follows: -->
 
 CREATE TABLE STATION (
   ID number,
@@ -11,19 +7,3 @@ CREATE TABLE STATION (
   LAT_N number,
   LONG_W number
 )
-
-SET @MAX_LAT = (SELECT MAX(LAT_N)
-FROM STATION
-WHERE LAT_N < 137.2345);
-
-SELECT ROUND(LONG_W, 4)
-FROM STATION
-WHERE LAT_N = @MAX_LAT;
-
--- REFACTORED
-
-SELECT ROUND(LONG_W, 4)
-FROM STATION
-WHERE LAT_N < 137.2345
-ORDER BY LAT_N DESC LIMIT 1;
-
---
+++ WeatherObservationStation18.sql
@@ -0,0 +1,33 @@
+-- Consider  and  to be two points on a 2D plane.
+
+--  happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+--  happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+--  happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+--  happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+CREATE TABLE STATION (
+  ID number,
+  CITY varchar2(21),
+  STATE varchar2,
+  LAT_N number,
+  LONG_W number
+)
+
+-- distance by pitagoras
+SELECT 
+    ROUND (
+        SQRT(
+          POWER( (MAX(LAT_N) - MIN(LAT_N)), 2) + 
+          POWER( (MAX(LONG_W) - MIN(LONG_W)), 2)
+          ) 
+        , 4)
+FROM STATION;
+
+-- Manhattan distance
+select round((max(lat_n) - min(lat_n)) + (max(long_w) - min(long_w)),4) 
+from station;
+
