212db961ea13be7474a71f592bca7a26b4f3486a
willianchu <willianchu@hotmail.com>
2022-10-26 11:59:06 -0700

    inner join two tables

--- WeatherObservationStation.sql
+++ WeatherObservationStation.sql
@@ -1,4 +1,10 @@
+-- Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
 
+-- Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+-- Input Format
+
+-- The STATION table is described as follows:
 
 CREATE TABLE STATION (
   ID number,
@@ -7,3 +13,13 @@ CREATE TABLE STATION (
   LAT_N number,
   LONG_W number
 )
+
+-- distance by Euclidean
+SELECT 
+    ROUND (
+        SQRT(
+          POWER( (MAX(LAT_N) - MIN(LAT_N)), 2) + 
+          POWER( (MAX(LONG_W) - MIN(LONG_W)), 2)
+          ) 
+        , 4)
+FROM STATION;
