04e1e98935ecfc013f34292fb574a1b5653a69af
willianchu <willianchu@hotmail.com>
2022-11-01 15:33:03 -0700

    Duplicate join same table

---
+++ placements.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Students (
+    ID INTEGER NOT NULL,
+    Name String NOT NULL
+);
+
+CREATE TABLE Friends (
+    ID INTEGER NOT NULL,
+    Friend_ID INTEGER NOT NULL
+);
+
+CREATE TABLE Packages (
+    ID INTEGER NOT NULL,
+    Salary Float NOT NULL
+);
