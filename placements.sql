a0c1ede94e315205fc5f8d54d579e9c5540a9a74
willianchu <willianchu@hotmail.com>
2022-11-02 21:04:10 -0700

    solved

--- placements.sql
+++ placements.sql
@@ -3,12 +3,25 @@ CREATE TABLE Students (
     Name String NOT NULL
 );
 
+-- Only the best friend
 CREATE TABLE Friends (
     ID INTEGER NOT NULL,
     Friend_ID INTEGER NOT NULL
 );
 
+-- Salary is in $ thousands per month
 CREATE TABLE Packages (
     ID INTEGER NOT NULL,
     Salary Float NOT NULL
 );
+
+SELECT Name
+FROM Students
+INNER JOIN Friends
+ON Students.ID = Friends.ID
+INNER JOIN Packages AS StudentPackage
+ON Friends.ID = StudentPackage.ID
+INNER JOIN Packages AS FriendPackage
+ON Friends.Friend_ID = FriendPackage.ID
+WHERE StudentPackage.Salary < FriendPackage.Salary
+ORDER BY FriendPackage.Salary;
