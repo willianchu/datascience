f260b72b57c0fb7c0e72933029b05d96d44724dc
willianchu <willianchu@hotmail.com>
2022-11-04 19:44:13 -0700

    first Select converting grades

---
+++ TheReport.sql
@@ -0,0 +1,17 @@
+-- You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+
+CREATE TABLE Students (ID INTEGER, Name TEXT, Marks INTEGER);
+CREATE TABLE Grades (ID INTEGER, Min_Mark INTEGER, Max_Mark INTEGER);
+
+--  generate a report containing three columns: Name, Grade and Mark. 
+-- we doesn't want the NAMES of those students who received a grade lower than 8.
+-- The report must be in descending order by grade 
+-- If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically.
+-- If the grade is lower than 8, use "NULL" as their name and list them by their marks in descending order.
+-- If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+SELECT 
+  Name, Grade, (SELECT Grade FROM Grades WHERE s.Marks >= Min_Mark AND s.Marks <= MaxMark) AS Marks
+FROM
+  Students s;
+
