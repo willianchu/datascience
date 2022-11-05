970470fd350c1fb4c632831754ad16d288fa57d5
willianchu <willianchu@hotmail.com>
2022-11-04 19:00:58 -0700

    ambiguos h_id

---
+++ ContestLeaderboard.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Hackers(
+  hacker_id INT NOT NULL,
+  name VARCHAR(255)
+);
+
+CREATE TABLE Submissions(
+  submission_id INT NOT NULL,
+  hacker_id INT NOT NULL,
+  challenge_id INT NOT NULL,
+  score INT NOT NULL
+);
+
+SELECT 
+    h.hacker_id, name,
+    SUM(score) AS total_score
+FROM
+    Hackers h
+    INNER JOIN Submissions s
+    ON h.hacker_id = s.hacker_id
+GROUP BY
+    hacker_id, name
+ORDER BY
+    total_score DESC, hacker_id ASC;
