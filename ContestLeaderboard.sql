55d75e8e0089baaa975102cc0654ecf79f994060
willianchu <willianchu@hotmail.com>
2022-11-04 19:24:13 -0700

    generate first a small table and then join names

--- ContestLeaderboard.sql
+++ ContestLeaderboard.sql
@@ -10,21 +10,33 @@ CREATE TABLE Submissions(
   score INT NOT NULL
 );
 
-SELECT 
-    hacker_id, name,
-    total_score
-FROM
-(SELECT 
-    h.hacker_id, name,
-    SUM(score) AS total_score
-FROM
-    Hackers h
-    INNER JOIN Submissions s
-    ON h.hacker_id = s.hacker_id
-GROUP BY
-    hacker_id, name
-ORDER BY
-    total_score DESC, hacker_id ASC) AS t
-WHERE
-    total_score <> 0;
+-- SELECT 
+--     hacker_id, name,
+--     total_score
+-- FROM
+-- (SELECT 
+--     h.hacker_id, name,
+--     SUM(score) AS total_score
+-- FROM
+--     Hackers h
+--     INNER JOIN Submissions s
+--     ON h.hacker_id = s.hacker_id
+-- GROUP BY
+--     hacker_id, name
+-- ORDER BY
+--     total_score DESC, hacker_id ASC) AS t
+-- WHERE
+--     total_score <> 0;
 
+-- The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. Exclude all hackers with a total score of  from your result.
+
+SELECT sc.hacker_id, name, sum(mscore)
+FROM (
+        SELECT hacker_id, challenge_id 
+              , max(score) as mscore
+        FROM SUBMISSIONS
+        GROUP by hacker_id, challenge_id
+        Having max(score) > 0 ) sc
+INNER JOIN hackers as ha  ON sc.hacker_id = ha.hacker_id
+GROUP BY sc.hacker_id, name
+ORDER by sum(mscore) desc, sc.hacker_id;
