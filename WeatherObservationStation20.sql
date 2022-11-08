b74c1f979456d9c8a1262747580002975a30a3ff
willianchu <willianchu@hotmail.com>
2022-11-07 21:52:52 -0800

    version with median formula

--- WeatherObservationStation20.sql
+++ WeatherObservationStation20.sql
@@ -6,9 +6,10 @@ CREATE TABLE STATION (
     LONG_W DECIMAL(9,6) NOT NULL,
 );
 
--- round median of (lat_N) to 4 decimal places
+-- A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
 
-SELECT
-    ROUND(AVG(LAT_N), 4)
-FROM
-    STATION;
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+SELECT LAT_N FROM STATION ORDER BY LAT_N LIMIT (SELECT COUNT(LAT_N) / 2 FROM STATION);
+
+(SELECT ROUND(COUNT(LAT_N) / 2) FROM STATION)
