e876c28ebef0016f3744525c140d95bdd7f51e57
willianchu <willianchu@hotmail.com>
2022-12-29 09:32:55 -0800

    version2

--- PrintPrimeNumbers.sql
+++ PrintPrimeNumbers.sql
@@ -1,16 +1,28 @@
 -- Write a query to print all prime numbers less than or equal to 100. Print your result on a single line, and use the ampersand (&) character as your separator (instead of a space).
 
-WITH RECURSIVE cte (Number) AS (
-    SELECT 1            -- base case returns 1
-    UNION ALL
-    SELECT Number + 1   -- recursive case returns 1 + previous value
-    FROM cte
-    WHERE Number < 100
+-- WITH RECURSIVE cte (Number) AS (
+--     SELECT 1            -- base case returns 1
+--     UNION ALL
+--     SELECT Number + 1   -- recursive case returns 1 + previous value
+--     FROM cte
+--     WHERE Number < 100
+-- )
+
+-- SELECT concat("&",Number) 
+-- FROM cte
+-- where ((Number * Number ) / (Number - 1)) <> 0
+-- ORDER BY Number;
+
+with x(val) as (select 1 val from dual 
+                union all 
+                select val+1 from x where val < 100)
+,primes as 
+(select val
+from (                
+select x.val,x1.val as divisor,mod(x.val,x1.val)  as remaindr
+from x
+join x x1 on x1.val <= x.val)
+group by val
+having sum(case when remaindr = 0 then 1 else 0 end) = 2
 )
-
-SELECT concat("&",Number) 
-FROM cte
-where ((Number * Number ) / (Number - 1)) <> 0
-ORDER BY Number;
-
-
+select * from primes
