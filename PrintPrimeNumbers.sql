176cfd423fc3355892fab241ac07f37795da65cc
willianchu <willianchu@hotmail.com>
2022-12-29 09:10:53 -0800

    counting to 100

--- PrintPrimeNumbers.sql
+++ PrintPrimeNumbers.sql
@@ -1,20 +1,15 @@
 -- Write a query to print all prime numbers less than or equal to 100. Print your result on a single line, and use the ampersand (&) character as your separator (instead of a space).
 
-Create Procedure PrintPrimeNumbers
-@startnum int,
-@endnum int
-AS 
-BEGIN
-Declare @a INT;
-Declare @i INT = 1
-(
-Select a = @startnum / 2;
-WHILE @i<@a
-BEGIN
-@startnum%(@a-@i)
-i=i+1;
+WITH RECURSIVE cte (Number) AS (
+    SELECT 1            -- base case returns 1
+    UNION ALL
+    SELECT Number + 1   -- recursive case returns 1 + previous value
+    FROM cte
+    WHERE Number < 100
 )
-END
 
+SELECT Number
+FROM cte
+ORDER BY Number;
 
 
