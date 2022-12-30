b65bdccab542b68f931ab73409215f6667a86a63
willianchu <willianchu@hotmail.com>
2022-12-30 09:42:52 -0800

    final version with comment

--- PrintPrimeNumbers.sql
+++ PrintPrimeNumbers.sql
@@ -1,28 +1,19 @@
 -- Write a query to print all prime numbers less than or equal to 100. Print your result on a single line, and use the ampersand (&) character as your separator (instead of a space).
 
--- WITH RECURSIVE cte (Number) AS (
---     SELECT 1            -- base case returns 1
---     UNION ALL
---     SELECT Number + 1   -- recursive case returns 1 + previous value
---     FROM cte
---     WHERE Number < 100
--- )
-
--- SELECT concat("&",Number) 
--- FROM cte
--- where ((Number * Number ) / (Number - 1)) <> 0
--- ORDER BY Number;
-
-with x(val) as (select 1 val from dual 
-                union all 
-                select val+1 from x where val < 100)
-,primes as 
-(select val
-from (                
-select x.val,x1.val as divisor,mod(x.val,x1.val)  as remaindr
-from x
-join x x1 on x1.val <= x.val)
-group by val
-having sum(case when remaindr = 0 then 1 else 0 end) = 2
-)
-select * from primes
+SELECT GROUP_CONCAT(NUMB SEPARATOR '&')
+FROM (
+    SELECT @num:=@num+1 as NUMB FROM -- count 2 to beyond 
+    information_schema.tables t1,
+    information_schema.tables t2,
+    (SELECT @num:=1) tmp
+) tempNum
+WHERE NUMB<=100 AND NOT EXISTS(
+		SELECT * FROM (
+			SELECT @nu:=@nu+1 as NUMA FROM
+			    information_schema.tables t1,
+			    information_schema.tables t2,
+			    (SELECT @nu:=1) tmp1
+			    LIMIT 1000
+			) tatata
+		WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
+	)
