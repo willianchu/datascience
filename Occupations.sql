56681ad66cf95a90c6de321dce1c0b603c401c26
willianchu <willianchu@hotmail.com>
2022-12-31 14:34:58 -0800

    Set problem

---
+++ Occupations.sql
@@ -0,0 +1,15 @@
+-- Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+-- Note: Print NULL when there are no more names corresponding to an occupation.
+
+-- Input Format
+
+-- The OCCUPATIONS table is described as follows:
+
+CREATE TABLE IF NOT EXISTS OCCUPATIONS(
+    NAME VARCHAR(20)
+    OCCUPATION VARCHAR(20),
+);
+
+-- Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+
