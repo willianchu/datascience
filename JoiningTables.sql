4f82f46c9009f9c588ae5be94f37d1a0ad6fef84
willianchu <willianchu@hotmail.com>
2023-01-03 20:07:39 -0800

    different approach

--- JoiningTables.sql
+++ JoiningTables.sql
@@ -15,9 +15,16 @@ CREATE TABLE Submissions
     FOREIGN KEY (hacker_id) REFERENCES Hackers(hacker_id)
 );
 
-SELECT submission_date, COUNT(DISTINCT Submissions.hacker_id) AS unique_hackers, Hackers.name
-FROM Submissions
-INNER JOIN Hackers
-ON Submissions.hacker_id = Hackers.hacker_id
-WHERE submission_date >= '2016-03-01' AND submission_date <= '2016-03-15'
-GROUP BY submission_date, Hackers.name;
+select 
+submission_date ,
+
+( SELECT COUNT(distinct hacker_id)  
+ FROM Submissions s2  
+ WHERE s2.submission_date = s1.submission_date AND    (SELECT COUNT(distinct s3.submission_date) FROM      Submissions s3 WHERE s3.hacker_id = s2.hacker_id AND s3.submission_date < s1.submission_date) = dateDIFF(s1.submission_date , '2016-03-01')) ,
+
+(select hacker_id  from submissions s2 where s2.submission_date = s1.submission_date 
+group by hacker_id order by count(submission_id) desc , hacker_id limit 1) as shit,
+(select name from hackers where hacker_id = shit)
+from 
+(select distinct submission_date from submissions) s1
+group by submission_date
