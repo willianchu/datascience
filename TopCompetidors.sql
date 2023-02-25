2901e38887a74d6bb5a663c78d25cd83c34bda33
willianchu <willianchu@hotmail.com>
2023-02-24 21:36:41 -0800

    Join table in Many to Many

--- TopCompetidors.sql
+++ TopCompetidors.sql
@@ -74,14 +74,11 @@ INSERT INTO Submissions (
   97431,90411,71055,30,
 )
 
-SELECT hacker_id, name
-FROM Hackers
-WHERE hacker_id IN (
-  SELECT hacker_id
-  FROM Submissions
-  WHERE score = (
-    SELECT MAX(score)
-    FROM Submissions
-    WHERE hacker_id = Submissions.hacker_id
-  )
-)
+SELECT H.hacker_id,H.name 
+FROM HACKERS H
+JOIN SUBMISSIONS S ON H.hacker_id = S.hacker_id
+JOIN CHALLENGES C ON C.challenge_id = S.challenge_id
+JOIN DIFFICULTY D ON D.score = S.score AND D.difficulty_level = C.difficulty_level
+GROUP BY H.hacker_id,H.name
+HAVING COUNT(C.challenge_id ) > 1
+ORDER BY COUNT(C.challenge_id) DESC, H.hacker_id ASC;
